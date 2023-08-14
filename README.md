16bb6b479b06d37f243a226207641dc62043040c
Jiaming Zhang <jzhang@brex.com>
2023-08-14 06:47:43 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+```bash
+source env/bin/activate
+```
---
+++ main.py
@@ -0,0 +1,45 @@
+from gtts import gTTS
+from pydub import AudioSegment
+
+import json
+import tempfile
+
+def create_audio(sentences, target_key="jp", translation_key="en", interval=1000, target_repeat=3, translation_repeat=1):
+    final_audio = AudioSegment.silent(duration=0)
+
+    # Load the "ding" sound
+    ding_sound = AudioSegment.from_file("ding.wav")
+
+    # Iterate through the sentences
+    for sentence in sentences:
+        target_text = sentence[target_key]
+        translation_text = sentence[translation_key]
+
+        # Convert target language text to speech
+        target_tts = gTTS(text=target_text, lang="ja")
+        with tempfile.NamedTemporaryFile(delete=False) as target_file:
+            target_tts.save(target_file.name)
+            target_audio = AudioSegment.from_mp3(target_file.name)
+
+        # Convert translation text to speech
+        translation_tts = gTTS(text=translation_text, lang="en")
+        with tempfile.NamedTemporaryFile(delete=False) as translation_file:
+            translation_tts.save(translation_file.name)
+            translation_audio = AudioSegment.from_mp3(translation_file.name)
+
+        # Repeat and combine the audio
+        combined_audio = target_audio * target_repeat + AudioSegment.silent(duration=interval)
+        combined_audio += translation_audio * translation_repeat + AudioSegment.silent(duration=interval)
+        combined_audio += target_audio
+
+        # Add to the final audio with a "ding" sound
+        final_audio += combined_audio + ding_sound
+
+    # Save the final audio
+    final_audio.export("final_audio.mp3", format="mp3")
+
+# Example usage
+with open('sentences.json') as f:
+    sentences = json.load(f)
+
+create_audio(sentences)
