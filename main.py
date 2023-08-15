40fac3e52f0cb6087495274cfb773885784818a9
Jiaming Zhang <jzhang@brex.com>
2023-08-15 07:06:49 -0700

    refactor(main.py): change long option names for input-audio, subtitle-file, and output-file to short options -i, --subtitle-file, and -o respectively for brevity refactor(main.py): change long option name verbose to short option -v for brevity

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def main():
 
     # Create a parser for the "fromaudio" command
     parser_fromaudio = subparsers.add_parser('fromaudio')
-    parser_fromaudio.add_argument('--input-audio', required=True, help='Input audio file to process.')
-    parser_fromaudio.add_argument('--subtitle-file', help='Subtitle file to process.')
-    parser_fromaudio.add_argument('--output-file', help='Output audio file. If not provided, it will be derived from the input audio file.')
+    parser_fromaudio.add_argument('-i', '--input-audio', required=True, help='Input audio file to process.')
+    parser_fromaudio.add_argument('--subtitle-file', help='Subtitle file to process. If not provided, it will be derived from the input audio file..')
+    parser_fromaudio.add_argument('-o', '--output-file', help='Output audio file. If not provided, it will be derived from the input audio file.')
     parser_fromaudio.add_argument('--transition-sound', required=True, help='Transition sound file.')
     parser_fromaudio.add_argument('--repeat-count', type=int, default=1, help='Number of times to repeat the audio.')
     parser_fromaudio.add_argument('--tr-lang', default='EN-US', help='Translation language. Default is "en".')
-    parser_fromaudio.add_argument('--verbose', action='store_true', help='Enable verbose output.')
+    parser_fromaudio.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output.')
     parser_fromaudio.add_argument('--tr-strategy', default='deepl', help='Translation strategy to use. Options are "deepl" and "fake". Default is "deepl".')
     parser_fromaudio.set_defaults(func=fromaudio_main)
 
