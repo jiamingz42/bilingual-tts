07701e7ace6d58fc5554c9ed6aa64c8476304d02
Jiaming Zhang <jzhang@brex.com>
2023-08-17 07:15:32 -0700

    aider: Added support for VTT subtitle format and converted them into the Subtitle dataclass.
    # Aider chat conversation:
    
    USER: In addition to SRT and ASS, support VTT subtitle format as well. After loading VTT subtitle, convert them into the Subtitle dataclass so they have consistent format.
    
    ASSISTANT: To support the VTT subtitle format, we need to use the `webvtt-py` library. If it's not already installed, you'll need to add it to your project's dependencies.
    
    The `webvtt-py` library provides a `webvtt` module that we can use to read VTT files. We can then convert the resulting captions into our `Subtitle` dataclass.
    
    Here are the changes we need to make to the `load_subtitle_file` function in the `dualang/subtitle_loader.py` file:
    
    1. Import the `webvtt` module at the top of the file.
    2. Add a new `elif` branch to the `load_subtitle_file` function to handle VTT files.
    
    Here are the proposed changes:
    
    ```python
    dualang/subtitle_loader.py
    <<<<<<< HEAD
    import os
    import pyass
    import pysrt
    
    from dataclasses import dataclass
    from typing import List
    =======
    import os
    import pyass
    import pysrt
    import webvtt
    
    from dataclasses import dataclass
    from typing import List
    >>>>>>> updated
    ```
    
    ```python
    dualang/subtitle_loader.py
    <<<<<<< HEAD
        elif subtitle_file.endswith(".ass"):
            with open(subtitle_file, encoding="utf_8_sig") as f:
                subtitle_data = pyass.load(f).events
            subtitles = [
                Subtitle(e.start.total_milliseconds(), e.end.total_milliseconds(), e.text)
                for e in subtitle_data
            ]
        else:
            print(
                f"Error: Unsupported subtitle file format. Only .srt and .ass are supported."
            )
            exit(1)
    =======
        elif subtitle_file.endswith(".ass"):
            with open(subtitle_file, encoding="utf_8_sig") as f:
                subtitle_data = pyass.load(f).events
            subtitles = [
                Subtitle(e.start.total_milliseconds(), e.end.total_milliseconds(), e.text)
                for e in subtitle_data
            ]
        elif subtitle_file.endswith(".vtt"):
            subtitle_data = webvtt.read(subtitle_file)
            subtitles = [
                Subtitle(s.start_in_seconds * 1000, s.end_in_seconds * 1000, s.text)
                for s in subtitle_data
            ]
        else:
            print(
                f"Error: Unsupported subtitle file format. Only .srt, .ass and .vtt are supported."
            )
            exit(1)
    >>>>>>> updated
    ```
    
    Please note that the `start_in_seconds` and `end_in_seconds` properties of a `Caption` object from the `webvtt` module return the start and end times in seconds, so we multiply by 1000 to convert them to milliseconds to match the format of our `Subtitle` dataclass.

--- dualang/subtitle_loader.py
+++ dualang/subtitle_loader.py
@@ -1,6 +1,7 @@
 import os
 import pyass
 import pysrt
+import webvtt
 
 from dataclasses import dataclass
 from typing import List
@@ -32,9 +33,15 @@ def load_subtitle_file(subtitle_file: str) -> List[Subtitle]:
             Subtitle(e.start.total_milliseconds(), e.end.total_milliseconds(), e.text)
             for e in subtitle_data
         ]
+    elif subtitle_file.endswith(".vtt"):
+        subtitle_data = webvtt.read(subtitle_file)
+        subtitles = [
+            Subtitle(s.start_in_seconds * 1000, s.end_in_seconds * 1000, s.text)
+            for s in subtitle_data
+        ]
     else:
         print(
-            f"Error: Unsupported subtitle file format. Only .srt and .ass are supported."
+            f"Error: Unsupported subtitle file format. Only .srt, .ass and .vtt are supported."
         )
         exit(1)
     print(f"Loaded subtitle file: {len(subtitles)} lines")
