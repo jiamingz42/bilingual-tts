15ea8b5ce13e6352afcf5d3599ebe5abab0f2085
Jiaming Zhang <jzhang@brex.com>
2023-08-17 07:17:04 -0700

    aider: Update the arg description to support VTT format.
    # Aider chat conversation:
    
    USER: Update the arg description about supporting VTT.
    
    ASSISTANT: To update the argument description for the `--subtitle-file` argument in the `main.py` file, we need to modify the `help` string for that argument.
    
    Here is the proposed change:
    
    ```python
    main.py
    <<<<<<< HEAD
        parser_fromaudio.add_argument(
            "-s",
            "--subtitle-file",
            help="Subtitle file to process. Supports .srt and .ass formats. If not provided, it will be derived from the input audio file.",
        )
    =======
        parser_fromaudio.add_argument(
            "-s",
            "--subtitle-file",
            help="Subtitle file to process. Supports .srt, .ass and .vtt formats. If not provided, it will be derived from the input audio file.",
        )
    >>>>>>> updated
    ```

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     parser_fromaudio.add_argument(
         "-s",
         "--subtitle-file",
-        help="Subtitle file to process. Supports .srt and .ass formats. If not provided, it will be derived from the input audio file.",
+        help="Subtitle file to process. Supports .srt, .ass and .vtt formats. If not provided, it will be derived from the input audio file.",
     )
     parser_fromaudio.add_argument(
         "-o",
