f93b859e9e3ff6041ed2cc02c62dcf7603f284b6
Jiaming Zhang <jzhang@brex.com>
2023-08-18 06:59:43 -0700

    aider: Update the unit tests for `subtitle_loader`: Instead of reading from a file, write the content of the subtitle as string, write them into a temp file and then read from them, and validate whether the function parse the right start, end & text field from the file for all three subtitle types.
    # Aider chat conversation:
    
    USER: Update the unit tests for `subtitle_loader`: Instead of reading from a file, write the content of the subtitle as string, write them into a temp file and then read from them. And then validate whether the function parse the right start, end & text field from the file. Do it for all three subtitle types.
    
    ASSISTANT: To update the unit tests for `subtitle_loader`, we will need to modify the `tests/test_subtitle_loader.py` file. We will create temporary files for each subtitle type, write the subtitle content to these files, and then pass these files to the `load_subtitle_file` function. We will then validate whether the function correctly parses the start, end, and text fields from the file.
    
    Here are the changes we need to make:
    
    <source>python
    tests/test_subtitle_loader.py
    <<<<<<< HEAD
        def setUp(self):
            self.srt_file = 'tests/mock_files/mock.srt'
            self.ass_file = 'tests/mock_files/existing_mock.ass'
            self.vtt_file = 'tests/mock_files/mock.vtt'
    
        def test_load_srt_file(self):
            subtitles = load_subtitle_file(self.srt_file)
            self.assertIsInstance(subtitles, list)
            self.assertIsInstance(subtitles[0], Subtitle)
    
        def test_load_ass_file(self):
            subtitles = load_subtitle_file(self.ass_file)
            self.assertIsInstance(subtitles, list)
            self.assertIsInstance(subtitles[0], Subtitle)
    
        def test_load_vtt_file(self):
            subtitles = load_subtitle_file(self.vtt_file)
            self.assertIsInstance(subtitles, list)
            self.assertIsInstance(subtitles[0], Subtitle)
    =======
        def test_load_srt_file(self):
            srt_content = "1\n00:00:01,000 --> 00:00:02,000\nThis is a test."
            with tempfile.NamedTemporaryFile(suffix=".srt", delete=False) as temp:
                temp.write(srt_content.encode())
                temp.close()
                subtitles = load_subtitle_file(temp.name)
                self.assertIsInstance(subtitles, list)
                self.assertEqual(subtitles[0].start, 1000)
                self.assertEqual(subtitles[0].end, 2000)
                self.assertEqual(subtitles[0].text, "This is a test.")
                os.remove(temp.name)
    
        def test_load_ass_file(self):
            ass_content = "[Events]\nDialogue: 0,0:00:01.00,0:00:02.00,Default,,0,0,0,,This is a test."
            with tempfile.NamedTemporaryFile(suffix=".ass", delete=False) as temp:
                temp.write(ass_content.encode())
                temp.close()
                subtitles = load_subtitle_file(temp.name)
                self.assertIsInstance(subtitles, list)
                self.assertEqual(subtitles[0].start, 1000)
                self.assertEqual(subtitles[0].end, 2000)
                self.assertEqual(subtitles[0].text, "This is a test.")
                os.remove(temp.name)
    
        def test_load_vtt_file(self):
            vtt_content = "00:00:01.000 --> 00:00:02.000\nThis is a test."
            with tempfile.NamedTemporaryFile(suffix=".vtt", delete=False) as temp:
                temp.write(vtt_content.encode())
                temp.close()
                subtitles = load_subtitle_file(temp.name)
                self.assertIsInstance(subtitles, list)
                self.assertEqual(subtitles[0].start, 1000)
                self.assertEqual(subtitles[0].end, 2000)
                self.assertEqual(subtitles[0].text, "This is a test.")
                os.remove(temp.name)
    >>>>>>> updated
    </source>
    
    Please note that we are using the `tempfile` module to create temporary files. We are also using the `os` module to remove the temporary files after we are done with them.
    
    Signed-off-by: Jiaming Zhang <jzhang@brex.com>

--- tests/test_subtitle_loader.py
+++ tests/test_subtitle_loader.py
@@ -1,27 +1,46 @@
 import os
 import unittest
+import tempfile
+
 from dualang.subtitle_loader import load_subtitle_file, Subtitle
 
-class TestSubtitleLoader(unittest.TestCase):
-    def setUp(self):
-        self.srt_file = 'tests/mock_files/mock.srt'
-        self.ass_file = 'tests/mock_files/existing_mock.ass'
-        self.vtt_file = 'tests/mock_files/mock.vtt'
 
+class TestSubtitleLoader(unittest.TestCase):
     def test_load_srt_file(self):
-        subtitles = load_subtitle_file(self.srt_file)
-        self.assertIsInstance(subtitles, list)
-        self.assertIsInstance(subtitles[0], Subtitle)
+        srt_content = "1\n00:00:01,000 --> 00:00:02,000\nThis is a test."
+        with tempfile.NamedTemporaryFile(suffix=".srt", delete=False) as temp:
+            temp.write(srt_content.encode())
+            temp.close()
+            subtitles = load_subtitle_file(temp.name)
+            self.assertIsInstance(subtitles, list)
+            self.assertEqual(subtitles[0].start, 1000)
+            self.assertEqual(subtitles[0].end, 2000)
+            self.assertEqual(subtitles[0].text, "This is a test.")
+            os.remove(temp.name)
 
     def test_load_ass_file(self):
-        subtitles = load_subtitle_file(self.ass_file)
-        self.assertIsInstance(subtitles, list)
-        self.assertIsInstance(subtitles[0], Subtitle)
+        ass_content = "[Events]\nDialogue: 0,0:00:01.00,0:00:02.00,Default,,0,0,0,,This is a test."
+        with tempfile.NamedTemporaryFile(suffix=".ass", delete=False) as temp:
+            temp.write(ass_content.encode())
+            temp.close()
+            subtitles = load_subtitle_file(temp.name)
+            self.assertIsInstance(subtitles, list)
+            self.assertEqual(subtitles[0].start, 1000)
+            self.assertEqual(subtitles[0].end, 2000)
+            self.assertEqual(subtitles[0].text, "This is a test.")
+            os.remove(temp.name)
 
     def test_load_vtt_file(self):
-        subtitles = load_subtitle_file(self.vtt_file)
-        self.assertIsInstance(subtitles, list)
-        self.assertIsInstance(subtitles[0], Subtitle)
+        vtt_content = "00:00:01.000 --> 00:00:02.000\nThis is a test."
+        with tempfile.NamedTemporaryFile(suffix=".vtt", delete=False) as temp:
+            temp.write(vtt_content.encode())
+            temp.close()
+            subtitles = load_subtitle_file(temp.name)
+            self.assertIsInstance(subtitles, list)
+            self.assertEqual(subtitles[0].start, 1000)
+            self.assertEqual(subtitles[0].end, 2000)
+            self.assertEqual(subtitles[0].text, "This is a test.")
+            os.remove(temp.name)
 
 if __name__ == '__main__':
     unittest.main()
