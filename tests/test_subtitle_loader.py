cd16f275fb1696f98633baa94b7368a210e45ab3
Jiaming Zhang <jzhang@brex.com>
2023-08-18 06:53:46 -0700

    aider: Add unit tests for `dualang/subtitle_loader.py`.
    # Aider chat conversation:
    
    USER: Add unit tests `dualang/subtitle_loader.py`
    
    ASSISTANT: To add unit tests for `dualang/subtitle_loader.py`, we will need to create a new test file `tests/test_subtitle_loader.py`.
    
    In this file, we will import the necessary modules and write test cases for the `load_subtitle_file` function. We will need to create mock subtitle files in different formats (.srt, .ass, .vtt) and test if the function correctly loads these files.
    
    Here is the change:
    
    <source>python
    tests/test_subtitle_loader.py
    <<<<<<< HEAD
    # This is an empty file
    =======
    import os
    import unittest
    from dualang.subtitle_loader import load_subtitle_file, Subtitle
    
    class TestSubtitleLoader(unittest.TestCase):
        def setUp(self):
            self.srt_file = 'tests/mock_files/mock.srt'
            self.ass_file = 'tests/mock_files/mock.ass'
            self.vtt_file = 'tests/mock_files/mock.vtt'
    
        def test_load_srt_file(self):
            subtitles = load_subtitle_file(self.srt_file)
            self.assertIsInstance(subtitles, list)
            self.assertIsInstance(subtitles[0], Subtitle)
    
        def test_load_ass_file(self):
            subtitles = load_subtitle_file(self.ass_file)
            self.assertIsInstance(subtitles, list)
            self.assertIsInstance(subtitles[0], Subtitle)
    
        def test_load_vtt_file(self):
            subtitles = load_subtitle_file(self.vtt_file)
            self.assertIsInstance(subtitles, list)
            self.assertIsInstance(subtitles[0], Subtitle)
    
    if __name__ == '__main__':
        unittest.main()
    >>>>>>> updated
    </source>
    
    This change creates a new test file `tests/test_subtitle_loader.py` with unit tests for the `load_subtitle_file` function. The tests check if the function correctly loads subtitle files in .srt, .ass, and .vtt formats.
    
    Please note that you will need to create the mock subtitle files in the `tests/mock_files/` directory for these tests to run. The mock files should contain valid subtitle data in the respective formats.

--- tests/test_subtitle_loader.py
+++ tests/test_subtitle_loader.py
@@ -1,1 +1,27 @@
-# This is an empty file
+import os
+import unittest
+from dualang.subtitle_loader import load_subtitle_file, Subtitle
+
+class TestSubtitleLoader(unittest.TestCase):
+    def setUp(self):
+        self.srt_file = 'tests/mock_files/mock.srt'
+        self.ass_file = 'tests/mock_files/mock.ass'
+        self.vtt_file = 'tests/mock_files/mock.vtt'
+
+    def test_load_srt_file(self):
+        subtitles = load_subtitle_file(self.srt_file)
+        self.assertIsInstance(subtitles, list)
+        self.assertIsInstance(subtitles[0], Subtitle)
+
+    def test_load_ass_file(self):
+        subtitles = load_subtitle_file(self.ass_file)
+        self.assertIsInstance(subtitles, list)
+        self.assertIsInstance(subtitles[0], Subtitle)
+
+    def test_load_vtt_file(self):
+        subtitles = load_subtitle_file(self.vtt_file)
+        self.assertIsInstance(subtitles, list)
+        self.assertIsInstance(subtitles[0], Subtitle)
+
+if __name__ == '__main__':
+    unittest.main()
