2b5d146653d2ffa025d694d6595d4f3b40aac5d2
Jiaming Zhang <jzhang@brex.com>
2023-08-20 21:00:29 -0700

    aider: Replace `create_audio()` call in `plaintext_main` function with `create_audio(sentences, args)` from `fromaudio` command.
    # Aider chat conversation:
    
    USER: `plaintext` command should use `create_audio` function from `fromaudio` command.
    
    ASSISTANT: To accomplish this, we need to modify the `main.py` file and the `plaintext.py` file in the `dualang/command/` directory. However, the `plaintext.py` file is currently read-only. Could you please add it to the chat so that we can make the necessary modifications?
    
    USER: I added these *read-write* files: dualang/command/plaintext.py
    
    ASSISTANT: Great, now we can proceed with the changes.
    
    First, we need to import the `create_audio` function from `fromaudio` in the `plaintext.py` file. Then, we will replace the call to `create_audio()` in the `plaintext_main` function with the imported function.
    
    Here are the changes:
    
    ```python
    dualang/command/plaintext.py
    <<<<<<< HEAD
    import os
    
    from dualang.split_japanese_text import split_japanese_text
    from dualang.command.fromtext import create_audio
    =======
    import os
    
    from dualang.split_japanese_text import split_japanese_text
    from dualang.command.fromaudio import create_audio
    >>>>>>> updated
    ```
    
    ```python
    dualang/command/plaintext.py
    <<<<<<< HEAD
        # Generate TTS audio segments for each sentence
        audio_segments = create_audio() # TODO
    =======
        # Generate TTS audio segments for each sentence
        audio_segments = create_audio(sentences, args)
    >>>>>>> updated
    ```
    
    Please note that the `create_audio` function from `fromaudio` might require different arguments than the one from `fromtext`. I assumed that it takes two arguments: the sentences and the args. If this is not the case, please let me know so I can adjust the code accordingly.

--- dualang/command/plaintext.py
+++ dualang/command/plaintext.py
@@ -1,7 +1,7 @@
 import os
 
 from dualang.split_japanese_text import split_japanese_text
-from dualang.command.fromtext import create_audio
+from dualang.command.fromaudio import create_audio
 
 
 def plaintext_main(args):
@@ -17,7 +17,7 @@ def plaintext_main(args):
         print(f" {i:03d} {s}")
 
     # Generate TTS audio segments for each sentence
-    audio_segments = create_audio() # TODO
+    audio_segments = create_audio(sentences, args)
 
     # # Combine the audio segments into a single audio file
     # output_file = args.output or os.path.splitext(args.input)[0] + '.mp3'
